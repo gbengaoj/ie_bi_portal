612fcb5a5471f81337da722add9d46de10c30ec7
Gbenga Ojo <gbengaoj@gmail.com>
2023-01-11 11:50:29 +0100

    setup authentication and custom theming

--- ie_bi_portal.py
+++ ie_bi_portal.py
@@ -1,14 +1,76 @@
 import streamlit as st
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
 #import pandas as pd
 #import numpy as np
 
 
-st.header('Welcome to Ikeja Electric BI Portal')
-st.subheader(''' This portal is created and maintained by DRIM ''')
+logo_url = "Ikeja-Electric-Logo2.png"
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+#st.image(logo_url, width=100)
+st.set_page_config(page_title="IE BI Dashboard", page_icon=logo_url, layout="wide")
 
-options = st.selectbox('Please Select', ['Technical Dashboard', 'Commercial&Technical Dashboard'])
 
-if options == 'Technical Dashboard':
-    st.markdown('<iframe title="IE-TECHNICAL DASHBOARD" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8fadb882-f320-4a52-86a0-7ef914fcfc65&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
-elif options == 'Commercial&Technical Dashboard':
-    st.markdown('<iframe title="Commercial & Technical Performance" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0c6d5e29-375f-4f6a-9d03-ab77c1b0c469&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
+
+
+
+
+
+
+
+# --- USER AUTHENTICATION ---
+names = ["Gbenga Ojo", "Sunny Okonkwo", "Feyisayo Adebiyi"]
+usernames = ["goojo", "SOkonkwo", "FAAdebiyi"]
+
+# Load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+
+credentials = {
+    "usernames":{
+        usernames[0]:{
+            "name":names[0],
+            "password":hashed_passwords[0]
+        },
+    usernames[1]:{
+        "name":names[1],
+        "password":hashed_passwords[1]
+    },
+    usernames[1]:{
+        "name":names[2],
+        "password":hashed_passwords[2]
+    }
+    }
+}
+
+authenticator = stauth.Authenticate(credentials,"bi_dashboard","abcdef",cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
+
+    st.header('Welcome to Ikeja Electric BI Portal')
+    st.subheader(''' This portal is created and maintained by DRIM ''')
+
+    authenticator.logout("Logout", "main")
+    st.write(f"Welcome {name}")
+
+    options = st.selectbox('Please Select', ['Technical Dashboard', 'Commercial&Technical Dashboard'])
+
+    if options == 'Technical Dashboard':
+        st.markdown('<iframe title="IE-TECHNICAL DASHBOARD" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8fadb882-f320-4a52-86a0-7ef914fcfc65&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
+    elif options == 'Commercial&Technical Dashboard':
+        st.markdown('<iframe title="Commercial & Technical Performance" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0c6d5e29-375f-4f6a-9d03-ab77c1b0c469&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
+
