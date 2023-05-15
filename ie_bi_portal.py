f9bf2f8c6d767a7d1ee4bf5609bb98798db53d8f
Gbenga Ojo <gbengaoj@gmail.com>
2023-05-15 13:59:35 +0100

    Added commercial dashboard

--- ie_bi_portal.py
+++ ie_bi_portal.py
@@ -82,9 +82,10 @@ options = st.selectbox('Please select a report:', ['Technical Dashboard', 'Comme
 
 if options == 'Technical Dashboard':
     st.markdown('<iframe title="IE-TECHNICAL DASHBOARD" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8fadb882-f320-4a52-86a0-7ef914fcfc65&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
-elif options == 'Commercial&Technical Dashboard':
-    st.markdown('<iframe title="Commercial & Technical Performance" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0c6d5e29-375f-4f6a-9d03-ab77c1b0c469&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
-
+#elif options == 'Commercial&Technical Dashboard':
+#    st.markdown('<iframe title="Commercial & Technical Performance" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0c6d5e29-375f-4f6a-9d03-ab77c1b0c469&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
+elif options == 'Commercial Dashboard':
+    st.markdown('<iframe title="Companywide Commercial Performance vdashboard UPDATED" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMWQ4MjJiMmItNmYwZS00ZGFiLWIxYWYtZjMzMzg3Y2MwMjNlIiwidCI6IjBjYTU1ZWIwLWVjYzItNGYwYS1hMTcxLWQ0ZTVlNmI1NzlkMyJ9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
 
 
 
