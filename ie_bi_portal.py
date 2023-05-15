868791f6445819056d3de6f97fa7f967aaac1a6e
Gbenga Ojo <gbengaoj@gmail.com>
2023-05-15 16:05:57 +0100

    Update options in code

--- ie_bi_portal.py
+++ ie_bi_portal.py
@@ -78,7 +78,7 @@ col2.header('Welcome to DRIM Portal')
 #st.subheader(''' This portal is created and maintained by DRIM ''')
 #st.write(f"Welcome {name}")
 
-options = st.selectbox('Please select a report:', ['Technical Dashboard', 'Commercial&Technical Dashboard'])
+options = st.selectbox('Please select a report:', ['Technical Dashboard', 'Commercial Dashboard'])
 
 if options == 'Technical Dashboard':
     st.markdown('<iframe title="IE-TECHNICAL DASHBOARD" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=8fadb882-f320-4a52-86a0-7ef914fcfc65&autoAuth=true&ctid=581816b2-c0be-4eaa-80e3-29e5f2486494" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
